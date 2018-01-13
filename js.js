70f4ec6eb3a2d67b5d7dd8a2b29545776e874240
DzmitryProkharau <33517308+DzmitryProkharau@users.noreply.github.com>
2018-01-13 12:47:11 +0300

    Update js.js

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ var fieldsArr = document.querySelectorAll('.field');
 var letterABC;
 var arrABC = ['a','b','c','d','e','f','g','h'];
 var checkersArr = document.createElement('div');
+var ind;
 ///////////////START  GAME//////////////////////////////////////////////////////
 for (var i = 0; i < fieldsArr.length; i++) {
   mainStep = 0;
@@ -14,7 +15,7 @@ for (var i = 0; i < fieldsArr.length; i++) {
     clone.id = 'child'+i;
     fieldsArr[i].appendChild(clone);
   }
-  if (i===0||i===21||i===27) {
+  if (i===0||i===21||i===26) {
     let clone = checkersArr.cloneNode(false);
     clone.className = 'checker wS ';
     clone.id = 'child'+i;
@@ -22,56 +23,60 @@ for (var i = 0; i < fieldsArr.length; i++) {
   }
 }
 ////////////////////////////////////////////////////////////////////////////////
-
-attacCheck();
-
-function attacCheck() {
-  var ind;
+for (var i = 0; i < fieldsArr.length; i++) {
+  fieldsArr[i].addEventListener('click', function (e) {
+    attacCheck(e.target.classList[1], e.target.offsetParent.id);
+  });
+}
+function attacCheck(x, y) {
   var field1, field2, field3, field4;
-  for (var i = 0; i < fieldsArr.length; i++) {
-    try {
-      if (fieldsArr[i].firstChild.classList[1] === 'wS') {
-        indexABC(fieldsArr[i].id);
-        field1 = arrABC[letterABC+1]+(+fieldsArr[i].id.charAt(1)+1);
-        field2 = arrABC[letterABC-1]+(+fieldsArr[i].id.charAt(1)+1);
-        field3 = arrABC[letterABC+1]+(+fieldsArr[i].id.charAt(1)-1);
-        field4 = arrABC[letterABC-1]+(+fieldsArr[i].id.charAt(1)-1);
+  try {
+    if (x === 'wS') {
+      indexABC(y);
+      field1 = arrABC[letterABC-1]+(+y.charAt(1)+1);
+      field2 = arrABC[letterABC+1]+(+y.charAt(1)+1);
+      field3 = arrABC[letterABC-1]+(+y.charAt(1)-1);
+      field4 = arrABC[letterABC+1]+(+y.charAt(1)-1);
+      if (
+      document.getElementById(field1).firstChild&&
+      document.getElementById(field1).firstChild.classList[1]||
+      document.getElementById(field2).firstChild&&
+      document.getElementById(field2).firstChild.classList[1]||
+      document.getElementById(field3).firstChild&&
+      document.getElementById(field3).firstChild.classList[1]||
+      document.getElementById(field4).firstChild&&
+      document.getElementById(field4).firstChild.classList[1] === 'dS') {
+        indexABC(field1);
+        field1 = arrABC[letterABC-1]+(+field1.charAt(1)+1);
+        indexABC(field2);
+        field2 = arrABC[letterABC+1]+(+field2.charAt(1)+1);
+        indexABC(field3);
+        field3 = arrABC[letterABC-1]+(+field3.charAt(1)-1);
+        indexABC(field4);
+        field4 = arrABC[letterABC+1]+(+field4.charAt(1)-1);
         if (
-         (document.getElementById(field1).firstChild.classList[1] === 'dS')||
-         (document.getElementById(field2).firstChild.classList[1] === 'dS')||
-         (document.getElementById(field3).firstChild.classList[1] === 'dS')||
-         (document.getElementById(field4).firstChild.classList[1] === 'dS')) {
-           indexABC(field1);
-           field1 = arrABC[letterABC+1]+(+field1.charAt(1)+1);
-           indexABC(field2);
-           field2 = arrABC[letterABC-1]+(+field2.charAt(1)+1);
-           indexABC(field3);
-           field3 = arrABC[letterABC+1]+(+field3.charAt(1)-1);
-           indexABC(field4);
-           field4 = arrABC[letterABC-1]+(+field4.charAt(1)-1);
-           if (document.getElementById(field1).childElementCount === 0||
-               document.getElementById(field2).childElementCount === 0||
-               document.getElementById(field3).childElementCount === 0||
-               document.getElementById(field4).childElementCount === 0) {
-                 ind = true;console.log('check');
-           }
+            !document.getElementById(field1).firstChild||
+            !document.getElementById(field2).firstChild||
+            !document.getElementById(field3).firstChild||
+            !document.getElementById(field4).firstChild ) {
+              ind = true;
         }
-      }else{
-        console.log("the attac it's inpossible");
       }
-    } catch (e) {}//try-catch
-  }
+      console.log(ind);
+    }else {
+      ind = false;
+      console.log(ind);
+    }
+  } catch (e) {}
 }
 
-function indexABC(x1) {
+function indexABC(x) {
   for (var i = 0; i < arrABC.length; i++) {
-    if (arrABC[i] === x1.charAt(0)) {
+    if (arrABC[i] === x.charAt(0)) {
       letterABC = i;
     }
   }
 }
-
-
 
 
 //
